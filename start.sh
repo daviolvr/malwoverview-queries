ef78abe33df5debf20bceefcdf6b305c7cbef6ed
Davi de Oliveira <olidavi031130@gmail.com>
2025-06-18 17:51:22 -0300

    chore: tirando criação do venv e install requirements do start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-echo "Criando ambiente virtual..."
-python3 -m venv .venv
-
 echo "Ativando ambiente virtual..."
 source .venv/bin/activate
-
-echo "Instalando dependências..."
-pip install -r requirements.txt
 
 # Verifica se o .env existe
 if [ ! -f .env ]; then
